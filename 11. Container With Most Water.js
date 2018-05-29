c12516c6b3bef8b182fc17a76fc277830221e12f
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-05-28 17:36:24 -0700

    sol 66 added

---
+++ 11. Container With Most Water.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    var max = 0,
+    temp = 0,
+    h = 0,
+    l = 0,
+    start = 0,
+    end = height.length -1;
+    for (var i = 0; i < height.length - 1; i++) {
+        l = end - start;
+        if (height[start] . height[end]) {
+            h = height[end];
+            end--;
+        } else {
+            h= height[start];
+            start++;
+        }
+        if (max < h * 1) {
+            max = h * 1;
+        }
+        }
+        return max;
+    }
+
+
